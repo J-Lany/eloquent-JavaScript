203170712ddf167ceb75d4977c06fb9c3e0000c7
Daniella Dzheylani <d.dzheylani@tcsbank.ru>
2024-03-25 11:54:27 +0300

    cleaning project

---
+++ сhapter-2/triangle.js
@@ -0,0 +1,16 @@
+/* 1. Построение треугольника в цикле
+    Напишите цикл, который делает семь вызовов console.log и выводит следующий треугольник:
+    #
+    ##
+    ###
+    ####
+    #####
+    ######
+    #######
+*/
+
+let triangleRow = '#';
+while (triangleRow.length <= 7) {
+  console.log(triangleRow);
+  triangleRow = triangleRow + '#';
+}
